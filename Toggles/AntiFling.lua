6739d8929845f063559d2c4325b2a51bdc23e58c
ios-boop <133600140+ios-boop@users.noreply.github.com>
2023-07-26 23:36:24 +0200

    Update AntiFling.lua

--- Toggles/AntiFling.lua
+++ Toggles/AntiFling.lua
@@ -2,19 +2,19 @@ local AntiFling = false
 
 if AntiFling = true then
 
-local localPlayer = game:GetService('Players').LocalPlayer;
-                local localCharacter = localPlayer.Character;
-                localCharacter:FindFirstChildOfClass('Humanoid').Health = 0;
-                local newCharacter = localPlayer.CharacterAdded:Wait();
-                local spoofFolder = Instance.new('Folder');
-                spoofFolder.Name = 'FULLY_LOADED_CHAR';
-                spoofFolder.Parent = newCharacter;
-                newCharacter:WaitForChild('RagdollConstraints'):Destroy();
-                local spoofValue = Instance.new('BoolValue', newCharacter);
-                spoofValue.Name = 'RagdollConstraints';
-                local name = game.Players.LocalPlayer.Name
-                local lol =    game.Workspace:WaitForChild(name)
-                local money = Instance.new("Folder",game.Players.LocalPlayer.Character);money.Name = "FULLY_LOADED_CHAR"
-                lol.Parent = game.Workspace.Players
+local a = game:GetService('Players').LocalPlayer;
+                local b = a.Character;
+                b:FindFirstChildOfClass('Humanoid').Health = 0;
+                local c = a.CharacterAdded:Wait();
+                local d = Instance.new('Folder');
+                d.Name = 'FULLY_LOADED_CHAR';
+                d.Parent = newCharacter;
+                c:WaitForChild('RagdollConstraints'):Destroy();
+                local e = Instance.new('BoolValue', c);
+                e.Name = 'RagdollConstraints';
+                local f = game.Players.LocalPlayer.Name
+                local g =    game.Workspace:WaitForChild(f)
+                local h = Instance.new("Folder",game.Players.LocalPlayer.Character);h.Name = "FULLY_LOADED_CHAR"
+                g.Parent = game.Workspace.Players
                 game.Players.LocalPlayer.Character:WaitForChild("BodyEffects")
                 game.Players.LocalPlayer.Character.BodyEffects.BreakingParts:Destroy()
