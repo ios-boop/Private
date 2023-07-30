dfdd32d1ff224f93fe0b1f8737a733e610eb5296
ios-boop <133600140+ios-boop@users.noreply.github.com>
2023-07-30 02:47:49 +0200

    Update AntiCheatBP.lua

--- Background/AntiCheatBP.lua
+++ Background/AntiCheatBP.lua
@@ -1,7 +1,1 @@
-local a,b,c=wait,game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame,function()
-end
-
-while a(0.005)do;b=b+b.lookVector*1
-b=b+b.lookVector*1
-b=b+b.lookVector*1
-end
+local a,b,c=wait,game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame,function()end while a(0.005)do;b=b+b.lookVector*1 b=b+b.lookVector*1 b=b+b.lookVector*1 end
